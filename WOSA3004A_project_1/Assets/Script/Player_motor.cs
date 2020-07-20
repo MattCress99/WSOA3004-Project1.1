1132f82c25c805bd72df0d068ed4addd1d826365
Ivy7493 <60912637+Ivy7493@users.noreply.github.com>
2020-07-20 13:19:23 +0200

    Added Movement

---
+++ WOSA3004A_project_1/Assets/Script/Player_motor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_motor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float _speed = 10f;
+    void Start()
+    {
+        
+    }
+
+    void zFix()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, 0);
+    }
+
+
+    void Motion()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+        zFix();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Motion();
+    }
+}
